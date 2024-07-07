1f6345fc60f79e354ba5e38b01aa25aaa4f69e38
Raf-Grigoryan <raf.grigoryan18@mail.ru>
2024-07-07 13:04:21 +0400

    Homework1

---
+++ src/homework/homework1/ReverseFor.java
@@ -0,0 +1,9 @@
+package homework.homework1;
+
+public class ReverseFor {
+    public static void main(String[] args) {
+        for (int i = 50; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
