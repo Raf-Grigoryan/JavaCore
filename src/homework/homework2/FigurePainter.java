71a05c31a59d141032f99131754061101f9ef62b
Raf-Grigoryan <raf.grigoryan18@mail.ru>
2024-07-12 11:57:24 +0400

    Homework2

--- src/homework/homework2/FigurePainter.java
+++ src/homework/homework2/FigurePainter.java
@@ -6,6 +6,7 @@ public class FigurePainter {
         secondFigure();
         thirdFigure();
         fourthFigure();
+        fifthFigure();
     }
 
     private static void firstFigure() {
@@ -43,17 +44,44 @@ public class FigurePainter {
             System.out.println();
         }
     }
-    private static void fourthFigure(){
+
+    private static void fourthFigure() {
         System.out.println("Fourth Figure");
         int count = 5;
-        for (int i = 0; i < 5 ; i++) {
-            for (int j = 0; j < i + 1 ; j++) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i + 1; j++) {
                 System.out.print("  ");
             }
-            for (int j = count; j > 0 ; j--) {
+            for (int j = count; j > 0; j--) {
                 System.out.print("* ");
             }
             count--;
+            System.out.println();
+        }
+    }
+
+    private static void fifthFigure() {
+        System.out.println("Fifth figure");
+        int count = 0;
+        int count2 = 5;
+        for (int i = 5; i > 0; i--) {
+            for (int j = i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < count + 1; j++) {
+                System.out.print("* ");
+            }
+            count++;
+            System.out.println();
+        }
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = count2; j > 0 ; j--) {
+                System.out.print(" *");
+            }
+            count2--;
             System.out.println();
         }
     }
