8f419bbdafcedf14bad87b23333b260b0e443c5d
Raf-Grigoryan <raf.grigoryan18@mail.ru>
2024-08-16 22:23:21 +0400

    homework6

---
+++ src/homework/homework6/DynmicArrayDemo.java
@@ -0,0 +1,15 @@
+package homework.homework6;
+
+public class DynmicArrayDemo {
+    public static void main(String[] args) {
+        DynamicArray dy = new DynamicArray();
+        for (int i = 0; i < 100; i++) {
+            dy.add(i);
+        }
+        dy.print();
+        dy.add(20,23);
+        dy.deleteByIndex(5);
+        System.out.println(dy.exists(5));
+        System.out.println(dy.getIndexByValue(56));
+    }
+}
